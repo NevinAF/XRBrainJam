3c8e3860c639b9ddb9efd058731aa0ad49cc748a
Max Harris <mharris382@gmail.com>
2021-06-26 21:00:55 -0400

    moved all map related scripts to subfolder

--- Assets/Scripts/Globe/GlobeRenderer.cs
+++ Assets/Scripts/Globe/GlobeRenderer.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-namespace DefaultNamespace
+namespace Globe
 {
     public class GlobeRenderer : MonoBehaviour
     {
---
+++ Assets/Scripts/Globe/WorldConfig.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Globe
+{
+    [CreateAssetMenu(menuName = "Globals/World Config")]
+    public class WorldConfig : ScriptableObject
+    {
+        public float worldMapRadius = 10;
+    
+    
+        private static WorldConfig _instance;
+
+        public static WorldConfig Instance
+        {
+            get
+            {
+                if (_instance==null)
+                {
+                    _instance = Resources.Load<WorldConfig>("World Config");
+                }
+                return _instance;
+            }
+        }
+    }
+}
